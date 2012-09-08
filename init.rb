22ab87869f1c9556abf29c21a406aba4173f4c47
E-Fir <ar@logiceditor.com>
2012-09-08 07:50:18 +0400

    initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_tickets_assistant do
+  name 'Redmine Tickets Assistant plugin'
+  author 'Alexey Romanov'
+  description 'Tickets manage assistant'
+  version '0.0.1'
+  url 'http://logiceditor.com'
+  author_url 'http://logiceditor.com'
+end
